6e0850aca4c793369b6ce8fe82b86a9aaca6531b
Vani Kulkarni <vani.kulkarni@irescglobal.com>
2021-02-15 14:02:59 +0530

    Commit: Changes for section 5

--- DatingApp/API/Startup.cs
+++ DatingApp/API/Startup.cs
@@ -39,10 +39,10 @@ namespace API
 			services.AddCors();
 			services.AddIdentityServices(_config);
 			
-			services.AddSwaggerGen(c =>
-			{
-				c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-			});
+			//services.AddSwaggerGen(c =>
+			//{
+			//	c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+			//});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,8 +51,8 @@ namespace API
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
-				app.UseSwagger();
-				app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+				//app.UseSwagger();
+				//app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
 			}
 
 			app.UseHttpsRedirection();
